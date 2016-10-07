16383e42efe26cb403e912f1aef959c680ede8ae
Donni Richasdy <donni.richasdy@gmail.com>
2016-10-07 21:55:58 +0700

    initial maven project

---
+++ src/main/java/com/richasdy/HelloLogging/App.java
@@ -0,0 +1,13 @@
+package com.richasdy.HelloLogging;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
