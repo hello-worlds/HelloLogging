08d90d620a30c318785cbf3457dbdd5cac81ef67
Donni Richasdy <donni.richasdy@gmail.com>
2016-10-08 22:26:37 +0700

    comment dblog and adding apache common logging interface

---
+++ src/main/java/CommonLogging/HelloCommonLogging.java
@@ -0,0 +1,44 @@
+package CommonLogging;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+//import org.apache.commons.logging.impl.Log4JLogger;
+
+public class HelloCommonLogging {
+
+	final static Log log = LogFactory.getLog(HelloCommonLogging.class);
+
+	// tidak perlu dengan ini
+	// final static Log4JLogger log = (Log4JLogger)
+	// LogFactory.getLog(HelloCommonLogging.class);
+
+	public static void main(String[] args) {
+
+		HelloCommonLogging obj = new HelloCommonLogging();
+		obj.runMe("richasdy");
+
+	}
+
+	private void runMe(String parameter) {
+
+		// log.setLevel(Level.WARN);
+
+		if (log.isTraceEnabled()) {
+			log.trace("This is trace : " + parameter);
+		}
+
+		if (log.isDebugEnabled()) {
+			log.debug("This is debug : " + parameter);
+		}
+
+		if (log.isInfoEnabled()) {
+			log.info("This is info : " + parameter);
+		}
+
+		log.warn("This is warn : " + parameter);
+		log.error("This is error : " + parameter);
+		log.fatal("This is fatal : " + parameter);
+
+	}
+
+}
